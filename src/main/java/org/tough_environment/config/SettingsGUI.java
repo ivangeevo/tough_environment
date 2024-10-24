e4b91e3da1f6da244f073c2cbdf805000bd6b031
Ivan Geevo <dbowgaming@gmail.com>
2024-10-24 12:44:49 +0300

    added new config options

--- src/main/java/org/tough_environment/config/SettingsGUI.java
+++ src/main/java/org/tough_environment/config/SettingsGUI.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.StringVisitable;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableTextContent;
 import org.tough_environment.ToughEnvironmentMod;
 
 import java.util.List;
@@ -18,11 +19,7 @@ public class SettingsGUI
     public static Screen createConfigScreen(Screen parent) {
         ConfigBuilder builder = ConfigBuilder.create()
                 .setParentScreen(parent).setTitle(Text.translatable("title.tough_environment.config"));
-        builder.setSavingRunnable(() -> {
-            ToughEnvironmentMod.getInstance().saveSettings();
-
-
-        });
+        builder.setSavingRunnable(() -> { ToughEnvironmentMod.getInstance().saveSettings(); });
 
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
@@ -31,10 +28,24 @@ public class SettingsGUI
         /** General Category**/
 
         general.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.tough_environment.hcPlayerMiningSpeed"), settingsCommon.hcPlayerMiningSpeed)
+                .startBooleanToggle(Text.translatable("config.tough_environment.hcPlayerMiningSpeed"), settingsCommon.hcPlayerMiningSpeed)
                 .setDefaultValue(true)
                 .setSaveConsumer(newValue -> settingsCommon.hcPlayerMiningSpeed = newValue)
+                .setTooltip(Text.translatable("config.tough_environment.tooltip.hcPlayerMiningSpeed"))
+                .build());
+
+        general.addEntry(entryBuilder
+                .startBooleanToggle(Text.translatable("config.tough_environment.stratificationToughness"), settingsCommon.stratificationToughness)
+                .setDefaultValue(true)
+                .setSaveConsumer(newValue -> settingsCommon.stratificationToughness = newValue)
+                .setTooltip(Text.translatable("config.tough_environment.tooltip.stratificationToughness"))
+                .build());
+
+        general.addEntry(entryBuilder
+                .startBooleanToggle(Text.translatable("config.tough_environment.blockBreakingRestrictions"), settingsCommon.blockBreakingRestrictions)
+                .setDefaultValue(true)
+                .setSaveConsumer(newValue -> settingsCommon.blockBreakingRestrictions = newValue)
+                .setTooltip(Text.translatable("config.tough_environment.tooltip.blockBreakingRestrictions"))
                 .build());
 
 
--- src/main/java/org/tough_environment/config/TESettings.java
+++ src/main/java/org/tough_environment/config/TESettings.java
@@ -5,10 +5,21 @@ public class TESettings
         public boolean hcMaterialDurability = true;
         public boolean hcPlayerMiningSpeed = true;
 
+        public boolean stratificationToughness = true;
+        public boolean blockBreakingRestrictions = true;
+
 
 
         public boolean isHardcorePlayerMiningSpeedEnabled() {
                 return hcPlayerMiningSpeed;
+        }
+
+        public boolean isStratificationToughnessEnabled() {
+                return stratificationToughness;
+        }
+
+        public boolean isBlockBreakingRestrictionsEnabled() {
+                return blockBreakingRestrictions;
         }
 
 }
