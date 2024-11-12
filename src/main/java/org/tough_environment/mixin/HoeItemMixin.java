5106343b8e28caa54fba03ec01b7d2e198e16bcb
Ivan Geevo <dbowgaming@gmail.com>
2024-11-12 20:49:46 +0200

    improved conditioning for limiting hoe right click function

--- src/main/java/org/tough_environment/mixin/HoeItemMixin.java
+++ src/main/java/org/tough_environment/mixin/HoeItemMixin.java
@@ -1,15 +1,20 @@
 package org.tough_environment.mixin;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.MiningToolItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.tough_environment.tag.ModTags;
+
 
 @Mixin(HoeItem.class)
 public abstract class HoeItemMixin extends MiningToolItem
@@ -24,6 +29,12 @@ public abstract class HoeItemMixin extends MiningToolItem
     // like dirt, coarse dirt, loose dirt, etc...
     @Inject(method = "useOnBlock", at = @At("HEAD"), cancellable = true)
     private void injectedUseOnBlock(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
-        cir.setReturnValue(ActionResult.FAIL);
+        BlockPos pos = context.getBlockPos();
+        BlockState state = context.getWorld().getBlockState(pos);
+
+        if ( state.isOf(Blocks.GRASS_BLOCK) || state.isIn(ModTags.Blocks.VANILLA_DIRT_BLOCKS))
+        {
+            cir.setReturnValue(ActionResult.FAIL);
+        }
     }
 }
