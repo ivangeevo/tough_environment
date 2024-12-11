ce69d5681078f701e4a1a8501a01fed4fa6ba499
Ivan Geevo <dbowgaming@gmail.com>
2024-12-11 20:15:38 +0200

    cleanup

--- src/main/java/org/tough_environment/mixin/HoeItemMixin.java
+++ src/main/java/org/tough_environment/mixin/HoeItemMixin.java
@@ -17,8 +17,7 @@ import org.tough_environment.tag.ModTags;
 
 
 @Mixin(HoeItem.class)
-public abstract class HoeItemMixin extends MiningToolItem
-{
+public abstract class HoeItemMixin extends MiningToolItem {
 
     public HoeItemMixin(ToolMaterial material, Settings settings) {
         super(material, BlockTags.HOE_MINEABLE, settings);
@@ -32,8 +31,7 @@ public abstract class HoeItemMixin extends MiningToolItem
         BlockPos pos = context.getBlockPos();
         BlockState state = context.getWorld().getBlockState(pos);
 
-        if ( state.isOf(Blocks.GRASS_BLOCK) || state.isIn(ModTags.Blocks.VANILLA_DIRT_BLOCKS))
-        {
+        if (state.isOf(Blocks.GRASS_BLOCK) || state.isIn(ModTags.Blocks.VANILLA_DIRT_BLOCKS)) {
             cir.setReturnValue(ActionResult.FAIL);
         }
     }
--- src/main/java/org/tough_environment/mixin/ItemEntityMixin.java
+++ src/main/java/org/tough_environment/mixin/ItemEntityMixin.java
@@ -9,18 +9,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.tough_environment.tag.ModTags;
 
 @Mixin(ItemEntity.class)
-public abstract class ItemEntityMixin
-{
+public abstract class ItemEntityMixin {
 
     // TODO: This has been added as a temporary "solution".
     //  It makes some items which usually merge (to reduce lag) to not merge.
-    //  Later on will be replaced with the custom dropping logic for incorrect item breaks.
+    //  figure out a less intrusive solution to pile items looking more spread out when dropping
     @Inject(method = "canMerge(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z", at = @At("HEAD"), cancellable = true)
     private static void injectedCustomMergeLogic(ItemStack stack1, ItemStack stack2, CallbackInfoReturnable<Boolean> cir)
     {
-        if (stack1.isIn(ModTags.Items.SHOULD_NOT_MERGE) || stack2.isIn(ModTags.Items.SHOULD_NOT_MERGE))
-        {
+        if (stack1.isIn(ModTags.Items.SHOULD_NOT_MERGE) || stack2.isIn(ModTags.Items.SHOULD_NOT_MERGE)) {
             cir.setReturnValue(false);
         }
+
     }
 }
